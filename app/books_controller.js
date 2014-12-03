f1aa46dc1d71091383487ea9058fb69548538295
MPMoughan <MattM@AmandoBs-MacBook-Pro-2.local>
2014-12-03 09:51:36 -0800

    update for angular lab

--- app/books_controller.js
+++ app/books_controller.js
@@ -4,6 +4,28 @@ bookly.controller('BooksController', function($scope) {
   // load books. They are defined in data.js (we've got no back end yet!)
   $scope.books = books;
 
-  //Code to manage cart goes here
+  $scope.total = 0;
+  $scope.items = [];
 
+  // function to add to that cart
+  $scope.addCart = function (book) {
+    $scope.items.push(book);
+    console.log($scope.items);
+    $scope.total += book.price;
+  };
+
+  // delete individual items
+  $scope.deleteItem = function(item) {
+    console.log(item);
+    var select = $scope.items.indexOf(item);
+    $scope.items.splice(select, 1);
+    $scope.total -= item.price;
+  };
+  //delete whole cart
+  $scope.deleteAll = function() {
+    console.log("clicked!")
+    $scope.items = [];
+    $scope.total = 0;
+
+  };
 });
